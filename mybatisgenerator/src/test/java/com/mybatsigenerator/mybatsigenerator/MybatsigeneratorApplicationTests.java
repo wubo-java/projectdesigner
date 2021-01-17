75d170565cbfe3407e565ddc826d3663c483e172
吴博 <wubo@szkingdom.com>
2021-01-17 10:43:35 +0800

    add by wubo 2020116 加上mybatis逆向生成工具，并生成对应代码。

---
+++ mybatisgenerator/src/test/java/com/mybatsigenerator/mybatsigenerator/MybatsigeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybatsigenerator.mybatsigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatsigeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
